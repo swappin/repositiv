7bb45d5ae7fa6210e125c941ba6d1673153e5add
your_username <andre.lljr@live.com>
2020-08-26 16:22:18 -0300

    Repository Filter at Home

--- lib/app/shared/components/logo_component.dart
+++ lib/app/shared/components/logo_component.dart
@@ -4,7 +4,8 @@ class LogoComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 120,
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 25),
       height: 27,
       child: Image.asset("assets/logo.png"),
     );
